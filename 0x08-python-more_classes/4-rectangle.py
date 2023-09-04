ea94c2871aae966f4717926d76eddb2790da7db0
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-09-04 13:45:39 -0700

    a class Rectangle that defines a rectangle by: (based on 3-rectangle.py)

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Define the rectangle."""
+
+
+class Rectangle:
+    """Represents a rectangle class."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrive the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrive the heigth of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates and return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculates and return the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a string representation of rectangle with #."""
+        if self.__width == 0 or self.__height ==0:
+            return ""
+        return "\n".join(["#" * self.__width] * self.__height)
+
+    def __repr__(self):
+        """Return a string representation of the object recreation."""
+        return (f"Rectangle({self.width}, {self.__height})")
