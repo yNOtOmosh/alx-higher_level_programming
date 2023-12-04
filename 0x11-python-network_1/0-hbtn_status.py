40aba676ab2c901050ebbc5ab6d7beb8bf5c5a50
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-12-04 02:20:32 -0800

    a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", utf8_content)
