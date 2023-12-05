869d06f980504ca0967eb50baeabd3b3e7ea7126
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-12-05 02:54:46 -0800

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+response = requests.get(url)
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
